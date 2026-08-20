544fe196b137960bfaa90bad5cf87a6f763e0796
Pratomo Adhi <47876896+pratomoadhi@users.noreply.github.com>
2026-08-20 09:45:31 +0700

    Add input cleaning and validation middleware

---
+++ sanitizer/middleware.js
@@ -0,0 +1,21 @@
+const inputCleaner = (req, res, next) => {
+    if (req.body.username) {
+        req.body.username = req.body.username.toLowerCase();
+    }
+
+    if (req.body.comment) {
+        req.body.comment = req.body.comment.replace(/<[^>]*>/g, "");
+    }
+
+    next();
+}
+
+const inputValidator = (req, res, next) => {
+    if (req.body.username && req.body.username.length >= 3) {
+        next();
+    } else {
+        res.redirect("/form?error=Username must be at least 3 characters.");
+    }
+}
+
+export { inputCleaner, inputValidator };
