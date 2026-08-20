f46f047509672249a014005895e5c67199d9facd
Pratomo Adhi <47876896+pratomoadhi@users.noreply.github.com>
2026-08-20 11:13:17 +0700

    Create server.js

---
+++ timestamp/server.js
@@ -0,0 +1,51 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors({ optionsSuccessStatus: 200 }));
+
+app.use(express.static("public"));
+
+app.get("/", (_req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// Do not change code above this line
+
+app.get("/api", (req, res) => {
+  const date = new Date();
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
+
+app.get("/api/:date", (req, res) => {
+  let date;
+
+  if (/^\d+$/.test(req.params.date)) {
+    date = new Date(Number(req.params.date));
+  } else {
+    date = new Date(req.params.date);
+  }
+
+  if (isNaN(date.getTime())) {
+    return res.json({
+      error: "Invalid Date"
+    });
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
+
+// Do not change code below this line
+
+const PORT = 8000;
+const listener = app.listen(PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
